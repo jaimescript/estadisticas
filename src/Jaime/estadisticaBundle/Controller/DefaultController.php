2c66c9a7017181200935fe5aef2221490ab8a1c4
Jaime Escobedo <broadwarx@gmail.com>
2015-07-06 17:08:24 -0500

    prueba , modificacion de controller

---
+++ src/Jaime/estadisticaBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Jaime\estadisticaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('JaimeestadisticaBundle:Default:index.html.twig', array('name' => $name));
+    }
+    public function LoginAction()
+    {
+        return $this->render('JaimeestadisticaBundle:Default:Login.html.twig');
+
+    }
+}
---
+++ src/Jaime/estadisticaBundle/JaimeestadisticaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jaime\estadisticaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JaimeestadisticaBundle extends Bundle
+{
+}
